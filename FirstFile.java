499aa0542463c67b9cbae7d431e2c4d758361200
Poonam <rjakhmola88@gmail.com>
2019-12-05 15:52:04 +0530

    project manager

---
+++ FirstFile.java
@@ -0,0 +1,9 @@
+package 2
+
+public class FirstFile{
+public static void main(string[] args){
+System.out.println("this is first program")
+
+}
+
+}
---
+++ SecondF.java
@@ -0,0 +1,8 @@
+package selPackage;
+
+
+public SecondF {
+public static void main(String[] args) {
+	
+        System.out.println("value");      
+        
